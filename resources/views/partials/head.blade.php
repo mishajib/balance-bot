9491f150959633300dd51529ff9e624f99ee3888
mi-shajib <mishajib222@gmail.com>
2025-10-18 13:37:05 +0600

    Added og content

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -9,5 +9,12 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+{{--og content--}}
+<meta property="og:title" content="{{ $title ?? config('app.name') }}" />
+<meta property="og:description" content="{{ 'BalanceBot - Your Personal Finance Assistant' }}" />
+<meta property="og:image" content="{{ asset('images/thumbnail.png') }}" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="{{ url()->current() }}" />
+
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
